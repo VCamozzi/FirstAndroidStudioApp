ffae4fdae01ff11285c18238f290dd307f7a1b92
Valentin camozzi <camozzi@et.esiea.fr>
2020-04-29 12:42:29 +0200

    Appel serveur

--- app/src/main/java/com/example/leagueoflegendsapp/RestPokemonResponse.java
+++ app/src/main/java/com/example/leagueoflegendsapp/RestPokemonResponse.java
@@ -6,7 +6,7 @@ public class RestPokemonResponse {
 
     private int count;
     private String next;
-    private List<Pokemon> result;
+    private List<Pokemon> results;
 
 
     public int getCount() {
@@ -17,7 +17,7 @@ public class RestPokemonResponse {
         return next;
     }
 
-    public List<Pokemon> getResult() {
-        return result;
+    public List<Pokemon> getResults() {
+        return results;
     }
 }
